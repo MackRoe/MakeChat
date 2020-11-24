c8e15203d674cf8d2e8401f90572f17f99aee138
MackRoe <elaine.music@students.makeschool.com>
2020-11-23 19:12:16 -0500

    create public dir, public/index.js, and configure app.js to establish public dir

--- app.js
+++ app.js
@@ -9,9 +9,13 @@ io.on("connection", (socket) => {
   console.log("🔌 New user connected! 🔌");
 })
 
+//Express View Engine for Handlebars
 const exphbs  = require('express-handlebars');
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+
+//Establish public folder
+app.use('/public', express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('index.handlebars');
